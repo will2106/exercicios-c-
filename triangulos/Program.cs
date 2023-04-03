8ba80fb46e539c2c618b52a214d5028019c46206
william santos <willdev0904@gmail.com>
2023-04-03 10:45:16 -0300

    atividades

---
+++ triangulos/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine($"gigite o lado 1: ");
+string ladoUm = Console.ReadLine();
+
+Console.WriteLine($"gigite o lado 2: ");
+string ladoDois =  Console.ReadLine();
+
+Console.WriteLine($"gigite o lado 3: ");
+string ladoTres =  Console.ReadLine();
+
+if (ladoUm == ladoDois && ladoTres == ladoTres ) 
+
+{
+
+Console.WriteLine($"Triangulo equilatero! ");
+
+}
+
+
+if (ladoUm == ladoDois || ladoTres == ladoTres ) 
+
+{
+
+Console.WriteLine($"Triangulo equilatero! ");
+
+}
+
